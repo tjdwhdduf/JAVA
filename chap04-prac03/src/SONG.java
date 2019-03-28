9082f1ed04dc228c43a5e0f16157a2a22120528e
USER <USER@AMP-12>
2019-03-28 15:11:11 +0900

    Commit

---
+++ chap04-prac03/src/SONG.java
@@ -0,0 +1,27 @@
+
+public class SONG {
+		private String title;
+		private String artist;
+		private int year;
+		private String country;
+		
+		public SONG() {}
+		public SONG(String title, String artist, int year, String country){
+				this.title = title;
+				this.artist = artist;
+				this.year = year;
+				this.country = country;
+		}
+		
+		public void show(){
+				System.out.print(year + "년 ");
+				System.out.print(country + "국적의 ");
+				System.out.print(artist + "가 부른 ");
+				System.out.println(title);
+		}
+		public static void main(String[] args) {
+				SONG myFavorite = new SONG("Dancing Queen","ABBA",1978, "스웨덴");
+				
+				myFavorite.show();
+		}
+}
